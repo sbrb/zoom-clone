78a39383076025cd9da451f38c2a44b2c0673b2a
sbrb <info.sbriju@gmail.com>
2024-12-12 18:04:04 +0530

    done

--- providers/StreamClientProvider.tsx
+++ providers/StreamClientProvider.tsx
@@ -21,7 +21,7 @@ const StreamVideoProvider = ({ children }: { children: ReactNode }) => {
       apiKey: API_KEY,
       user: {
         id: user?.id,
-        name: user?.username || user?.id,
+        name: user?.fullName || user?.id,
         image: user?.imageUrl,
       },
       tokenProvider,
